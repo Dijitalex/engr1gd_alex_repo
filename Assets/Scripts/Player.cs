fcf132a9bf9ed562ca2dec89472256b782a154f2
Dijitalex <alex.dong.reno@gmail.com>
2025-10-02 14:49:04 -0700

    Week 2 complete!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Keywords: base and this
+    void Start()
+    {
+        Debug.Log(gameObject.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    float movementX;
+    float movementY;
+    [SerializeField] float speed = 6;
+
+    [SerializeField] Rigidbody2D rb; //remember default is private, serializefield will keep it private but accessible in the unity editor
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+        movementY = v.y;
+
+        Debug.Log(v);
+
+    }
+    void FixedUpdate()
+    {//As opposed to regular Update(), game physics will not be time depend on frames cough cough Touhou
+        float XmoveDistance = movementX * speed * Time.fixedDeltaTime;
+        //float YmoveDistance = movementY * speed * Time.fixedDeltaTime;
+
+        //transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
+        //rb.linearVelocity = new Vector2 (40*XmoveDistance, 40*YmoveDistance);
+        rb.linearVelocityX = XmoveDistance;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grnd"))//collision.gameObject.tag == "ground"
+        {
+            //rb.AddForce(new Vector2(5, 175));
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+    
+}
