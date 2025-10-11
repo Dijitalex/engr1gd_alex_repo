eb4b6863d098b96060b4b7e8520275a57ce6eb9a
Dijitalex <alex.dong.reno@gmail.com>
2025-10-10 19:07:43 -0700

    Added most of week 3 classwork

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,75 @@
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     float movementX;
     float movementY;
+    bool jumping = false;
+    bool touchGround;
+    bool facingRight = true;
+    private Animator animator;
     [SerializeField] float speed = 150; //W SPEED
-
+    [SerializeField] float jump = 10f;
     [SerializeField] Rigidbody2D rb; //remember default is private, serializefield will keep it private but accessible in the unity editor
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        animator.SetBool("walking", movementX != 0f);
+        // if (movementX < 0 && facingRight)
+        // {
+        //     transform.localScale.x *= -1;
+        // }
     }
 
     void OnMove(InputValue value)
     {
-        Vector2 v = value.Get<Vector2>();
+        UnityEngine.Vector2 v = value.Get<UnityEngine.Vector2>();
 
         movementX = v.x;
         movementY = v.y;
 
+
         Debug.Log(v);
 
     }
+
+    void OnJump()
+    {
+        if (touchGround)
+        {
+            jumping = true;
+        }
+    }
     void FixedUpdate()
     {//As opposed to regular Update(), game physics will not be time depend on frames cough cough Touhou
-        float XmoveDistance = movementX * speed * Time.fixedDeltaTime;
-        float YmoveDistance = movementY * speed * Time.fixedDeltaTime;
+        float XmoveDistance = movementX * speed;
+        //float YmoveDistance = movementY * speed * Time.fixedDeltaTime;
 
-        transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
+        //transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
         //rb.linearVelocity = new Vector2 (40*XmoveDistance, 40*YmoveDistance);
-        //rb.linearVelocityX = XmoveDistance;
-    }
+        rb.linearVelocityX = XmoveDistance;
 
+        if (touchGround && jumping)
+        {
+            rb.AddForce(jump * UnityEngine.Vector2.up, ForceMode2D.Impulse);
+            jumping = false;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Grnd"))//collision.gameObject.tag == "ground"
         {
-            //rb.AddForce(new Vector2(5, 175));
+            touchGround = true;
         }
     }
     private void OnCollisionStay2D(Collision2D collision)
@@ -53,7 +78,10 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-
+        if (collision.gameObject.CompareTag("Grnd"))//collision.gameObject.tag == "ground"
+        {
+            touchGround = false;
+        }
     }
     
 }
